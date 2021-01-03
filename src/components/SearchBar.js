d7b06c8ac7b92e8f602105c3d8506ff33965dfca
mike <mikemcfaddin@gmail.com>
2021-01-02 20:48:55 -0500

    deployed on vercel and made changes to test redeployment

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,7 +17,7 @@ const SearchBar = ({ onTermSubmit }) => {
     <div className="search-bar ui segment">
       <form onSubmit={onSubmit} className="ui form">
         <div>
-          <label>Video Search</label>
+          <label>Search For A Video</label>
           <input 
             type="text" 
             value={term}
